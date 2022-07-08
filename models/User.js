cc2162a1cd77e161e3a3fb3d7e8aba7cfa2b7a10
katochsenthal <katoch.senthal@gmail.com>
2022-07-07 22:51:12 -0400

    added models

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    address: AddressSchema,
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+
+UserSchema.virtuals("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
